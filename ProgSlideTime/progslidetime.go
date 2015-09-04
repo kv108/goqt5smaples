36554a4cc83339ae410a18f1026abb30fb76c2a3
kv108 <crystal_83@bk.ru>
2015-09-04 12:25:35 +0000

    ok

--- ProgSlideTime/progslidetime.go
+++ ProgSlideTime/progslidetime.go
@@ -13,45 +13,65 @@ func main() {
 func ui_main() {
 	w := qt5.NewWidget()
 	defer w.Close()
-	w.SetGeometryv(10, 10, 500, 500)
+	w.SetGeometryv(10, 10, 500, 200)
 
 	chkbox := qt5.NewCheckBoxWithText("Таймер вкл/выкл")
 	chkbox.SetSizePolicyHV(0, 0)
-	var sz qt5.Size
-	sz.Height = 300
-	sz.Width = 300
+	timer := qt5.NewTimer()
+	timer.SetInterval(100)
+	timelab := qt5.NewLabelWithText("0")
+	timelab.SetFrameStyle(1)
 
+	timelab.SetSizePolicyHV(0, 0)
+
+	progress := qt5.NewScrollBar()
+	progress.SetRange(0, 100)
+	progress.SetEnabled(false)
+
+	slide1 := qt5.NewSlider()
+	slide1.SetRange(0, 100)
+
+	label1 := qt5.NewLabelWithText("0    ")
+	label1.SetSizePolicyHV(0, 0)
+	//chkbox.SetSizePolicyHV(0, 0)
+	//var sz qt5.Size 	sz.Height = 300 	sz.Width = 300
+	hbox := qt5.NewHBoxLayout()
 	vbox := qt5.NewVBoxLayout()
-	vbox.AddWidget(button2)
 
-	table := qt5.NewTableWidget()
-	table.SetRowCount(10)
-	table.SetColumnCount(10)
+	hbox.AddWidget(progress)
+	hbox.AddWidget(chkbox)
+	hbox.AddWidget(timelab)
 
-	vbox.AddWidget(tab)
+	vbox.AddLayout(hbox)
+	vbox.AddWidget(slide1)
+	vbox.AddWidget(label1)
 	w.SetLayout(vbox)
-	num := 1
-	//var ti *qt5.TableWidgetItem
+	num := 0
 
 	go func() {
 
-		button2.OnClicked(func() {
-			lab := strconv.Itoa(num)
-			tab.AddTab(qt5.NewFrame(), "вкладка "+lab, &icon)
-			num++
+		chkbox.OnStateChanged(func(st int) {
+			fmt.Printf("State changed on %d \n", st)
+			if st == 2 {
+				timer.Start()
+			}
+			if st == 0 {
+				timer.Stop()
+			}
 
 		})
+		timer.OnTimeout(func() {
+			fmt.Printf("Time=%d \n", num)
+			timelab.SetText(strconv.Itoa(num))
+			num = num + 1
+			progress.SetSliderPosition(num)
 
-		table.OnCellActivated(func(row int, column int) {
-			fmt.Printf("row=%d column=%d \n", row, column)
-			curit := table.Item(row, column)
-			if curit == nil {
-				ti := qt5.NewTableWidgetItem()
-				ti.SetText("NewItem")
-				table.SetItem(row, column, ti)
-			} else {
-				curit.SetText("ItemExist")
+			if num == 100 {
+				num = 0
 			}
+		})
+		slide1.OnValueChanged(func(val int) {
+			label1.SetText(strconv.Itoa(val))
 		})
 
 	}()
